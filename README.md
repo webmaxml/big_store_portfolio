a184f4b61f17c098a9d284e294bb0f44364f5f67
webmaxml <webmaxml@gmail.com>
2016-02-18 11:59:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## Big store
+My portfolio work, based on [PSD](http://kopatheme.com/freebies/big-store-free-ecommerce-psd-website-template/ "PSD link") provided by [kopatheme.com](http://kopatheme.com/ "kopatheme")
---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+'use strict';
+
+        var gulp = require('gulp'),
+           watch = require('gulp-watch'),
+       webserver = require('gulp-webserver'),
+    autoprefixer = require('gulp-autoprefixer'),
+         compass = require('gulp-compass'),
+            jade = require('gulp-jade');
+
+
+gulp.task('watch', function() {
+    watch( './src/sass/**/*.scss', function() {
+        gulp.start( 'sass' );
+    } );
+    watch( './src/jade/**/*.jade', function() {
+        gulp.start( 'jade' );
+    } );
+});
+
+gulp.task('sass', function () {
+    gulp.src('./src/sass/style.scss')
+    .pipe(compass({
+        css: 'dist/css',
+        sass: 'src/sass',
+        image: 'dist/img',
+        sourcemap: true,
+        require: ['susy']
+    }))
+    .on('error', function(error) {
+        console.log(error);
+        this.emit('end');
+    })
+    .pipe(autoprefixer({
+        browsers: ['last 15 versions'],
+        cascade: false
+    }))
+    .pipe(gulp.dest('./dist/css/'));
+});
+
+gulp.task( 'jade', function() {
+    gulp.src( './src/jade/*.jade' )
+    .pipe( jade({
+        pretty: true
+    }) )
+    .on('error', function(error) {
+        console.log(error);
+        this.emit('end');
+    })
+    .pipe( gulp.dest( './dist/' ) )
+} );
+
+gulp.task('webserver', function() {
+    gulp.src('dist')
+        .pipe(webserver({
+            livereload: true,
+            open: true
+        }));
+});
+
+gulp.task('default', ['webserver', 'watch']);
