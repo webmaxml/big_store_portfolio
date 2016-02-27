bea86f49925792b96a69fc3137cd1a6b8759a0a3
webmaxml <webmaxml@gmail.com>
2016-02-27 14:38:29 +0200

    products scrollbar configured & sale badge

--- dist/js/js.js
+++ dist/js/js.js
@@ -1,10 +1,10 @@
 jQuery( function( $ ) {
 
 	// shopping cart
-	var   $shoppingCart = $( document.getElementsByClassName( 'shopping-cart' )[0] ),
-		$shoppingBasket = $( document.getElementsByClassName( 'shopping-cart__basket' )[0] ),
-		$shoppingToggle = $( document.getElementsByClassName( 'shopping-cart__toggle' )[0] );
-		
+	var $shoppingCart = $( document.getElementsByClassName( 'shopping-cart' )[0] );
+	var $shoppingBasket = $( document.getElementsByClassName( 'shopping-cart__basket' )[0] );
+	var $shoppingToggle = $( document.getElementsByClassName( 'shopping-cart__toggle' )[0] );
+
 	$shoppingCart.on( 'click', '.shopping-cart__toggle', basketToggle );
 
 	function basketToggle() {
@@ -18,9 +18,9 @@ jQuery( function( $ ) {
 
 
 	// mobile-menu
-	var       $header = $( document.getElementsByClassName( 'header' )[0] ),
-		  $mobileMenu = $( document.getElementsByClassName( 'header__mobile-menu' )[0] ),
-		$mobileToggle = $( document.getElementsByClassName( 'header__mobile-toggle' )[0] );
+	var $header = $( document.getElementsByClassName( 'header' )[0] );
+	var $mobileMenu = $( document.getElementsByClassName( 'header__mobile-menu' )[0] );
+	var $mobileToggle = $( document.getElementsByClassName( 'header__mobile-toggle' )[0] );
 
 	$header.on( 'click', '.header__mobile-toggle', menuToggle );
 
@@ -33,16 +33,10 @@ jQuery( function( $ ) {
 		})
 	};
 
-	// $( window ).on( 'resize', onResize );
-
-	// function onResize( event ) {
-	// 	if ( this.innerWidth > 700 ) { return; }
-	// 	console.log( this.innerWidth );
-	// };
-
+	
 	// top slider
-	var     $topSlider = $( document.getElementsByClassName( 'top-slider' )[0] ),
-		$topSliderWrap = $( document.getElementsByClassName( 'main__slider-wrap' )[0] );
+	var $topSlider = $( document.getElementsByClassName( 'top-slider' )[0] );
+	var $topSliderWrap = $( document.getElementsByClassName( 'main__slider-wrap' )[0] );
 
 	$topSlider.owlCarousel({
 		singleItem: true,
@@ -63,8 +57,8 @@ jQuery( function( $ ) {
 	};
 
 	// featured slider
-	var $featuredSlider = $( document.getElementsByClassName( 'featured__content' )[0] ),
-		      $featured = $( document.getElementsByClassName( 'featured' )[0] );
+	var $featuredSlider = $( document.getElementsByClassName( 'featured__content' )[0] );
+	var $featured = $( document.getElementsByClassName( 'featured' )[0] );
 
 	$featuredSlider.owlCarousel({
 		singleItem: true,
@@ -84,4 +78,93 @@ jQuery( function( $ ) {
 		if ( isNext ) { $featuredSlider.trigger( 'owl.next' ); }
 	};
 
+	// new-products scrollbar
+
+	var $itemViewport = $( document.getElementsByClassName( 'new-products__content' )[0] );
+	var $itemBox = $( document.getElementsByClassName( 'new-products__item-box' )[0] );
+	var $scrollbar = $( document.getElementsByClassName( 'new-products__scrollbar' )[0] );
+
+	var itemCount = $itemBox.children().length;
+	var itemLength = 256;
+	var itemMargin = 24;
+
+	var itemBoxWidth = itemCount * ( itemLength + itemMargin ) - itemMargin;
+	var itemViewportWidth = $itemViewport.width();
+
+	var itemBoxMaxOffset = itemBoxWidth - itemViewportWidth;
+
+	$itemBox.css( 'width', itemBoxWidth );
+
+	$scrollbar.slider({
+		slide: itemBoxSlide,
+	});
+
+	function itemBoxSlide( event, obj ) {
+		$itemBox.css({
+			'marginLeft': ( -obj.value / 100 ) * itemBoxMaxOffset
+		});
+	};
+
+	var $scrollHandle = $( document.getElementsByClassName( 'ui-slider-handle' )[0] );
+	$scrollHandle.html('|||');
+
+	// sale-badge
+	var saleBadge = '<div class="sale-badge">Sale</div>';
+	var saleBadgewithBack = '<div class="sale-badge">Sale<div class="sale-badge__back-left"></div><div class="sale-badge__back-right"></div></div>';
+
+	var $itemsSale = $( document.getElementsByClassName( 'new-products__item--sale' ) );
+
+	if ( $itemsSale.length > 0 ) {
+		$itemsSale
+			.find('.new-products__img-box')
+			.append( saleBadgewithBack );
+	};
+	
+
+	// function debounce(func, wait, immediate) {
+	// 	var timeout;
+	// 	return function() {
+	// 		var later = function() {
+	// 			timeout = null;
+	// 		};
+	// 		var callNow = !timeout;
+	// 		clearTimeout(timeout);
+	// 		timeout = setTimeout(later, wait);
+	// 		if (callNow) func();
+	// 	};
+	// };
+
+	// function throttle(fn, threshhold) {
+	// 	var last,
+	// 	deferTimer;
+	// 	return function () {
+	// 		var context = this;
+	// 		var now = +new Date;
+	// 		var args = arguments;
+
+	// 		if (last && now < last + threshhold) {
+	// 			clearTimeout(deferTimer);
+	// 			deferTimer = setTimeout(function () {
+	// 				last = now;
+	// 				fn.apply(context, args);
+	// 			}, threshhold);
+	// 		} else {
+	// 			last = now;
+	// 			fn.apply(context, args);
+	// 		}
+	// 	};
+	// }
+
 } );
+
+// function myDebounce( fn, wait ) {
+// 	var timeout;
+// 	return function() {
+// 		var context = this, args = arguments;
+
+// 		clearTimeout( timeout );
+// 		timeout = setTimeout(function() {
+// 			fn.apply( context, arguments );
+// 		}, wait);
+// 	};
+// }
