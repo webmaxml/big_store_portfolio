bc08792ca12b95a59ef843ed5e0f591bea5005fb
webmaxml <webmaxml@gmail.com>
2016-03-03 14:06:58 +0200

    adding to cart feature

--- dist/js/js.js
+++ dist/js/js.js
@@ -7,12 +7,15 @@ jQuery( function( $ ) {
 
 
 	var $shoppingCart = $( document.getElementsByClassName( 'shopping-cart' )[0] );
-	var $shoppingBasket = $( document.getElementsByClassName( 'shopping-cart__basket' )[0] );
+	var $shoppingBasket = $( document.getElementsByClassName( 'shopping-basket' )[0] );
 	var $shoppingToggle = $( document.getElementsByClassName( 'shopping-cart__toggle' )[0] );
 
+	$( document ).on( 'click', '.btn-cart', productAdd );
 	$shoppingCart.on( 'click', '.shopping-cart__toggle', basketToggle );
 
 	function basketToggle() {
+		if ( $shoppingBasket.children().length == 0 ) { return; }
+
 		$shoppingBasket.slideToggle({
 			duration: 150,
 			complete: function() {
@@ -20,6 +23,75 @@ jQuery( function( $ ) {
 			}
 		});
 	};
+
+	function productAdd( event ) {
+		var $target = $( event.target );
+
+		if ( $target.hasClass( 'btn-cart--added' ) ) { return; }
+
+		var info = getItemInfo( $target );
+
+		var item = '<li class="shopping-basket__item">' + 
+						'<div class="shopping-basket__img-box">' +
+							'<a class="shopping-basket__link" href="' + info.itemSrc + '">' + 
+								'<img class="shopping-basket__img" src="' + info.imgSrc + '" alt="' + info.imgAlt + '">' +
+							'</a>' +
+						'</div>' +
+						'<div class="shopping-basket__item-name">' +
+							'<a class="shopping-basket__link" href="' + info.itemSrc + '">' +
+								info.name +
+							'</a>' +
+						'</div>' +
+						'<div class="shopping-basket__price">' +
+							'<span class="shopping-basket__current-currency">' + info.currency + '</span>' +
+							'<span class="shopping-basket__current-value">' + info.price + '</span>' +
+						'</div>' +
+						'<div class="shopping-basket__delete">' +
+							'<button class="shopping-basket__delete-btn" type="button">' +
+								'<i class="fa fa-close shopping-basket__delete-icon"></i>' +
+							'</button>' +
+						'</div>' +
+					'</li>';
+
+		$shoppingBasket.append( item );
+
+		btnCartChecked( $target );
+	};
+
+	function getItemInfo( $button ) {
+		var $item = $button.closest( 'li' );
+
+		var itemClass = $item.get(0)
+							 .classList[0]
+							 .split('__')[0];  // all items first class pattern - " 'class_name'__item "
+
+		var itemImgClass = '.'.concat( itemClass, '__img' );
+		var itemNameClass = '.'.concat( itemClass, '__item-name' );
+		var itemCurrencyClass = '.'.concat( itemClass, '__current-currency' );
+		var itemPriceClass = '.'.concat( itemClass, '__current-value' );
+
+		return {
+			itemSrc: $item.find( itemNameClass ).children( 'a' ).attr( 'href' ),
+			imgSrc: $item.find( itemImgClass ).attr( 'src' ),
+			imgAlt: $item.find( itemImgClass ).attr( 'alt' ),
+			name: $item.find( itemNameClass ).children( 'a' ).html(),
+			currency: $item.find( itemCurrencyClass ).html(),
+			price: $item.find( itemPriceClass ).html()
+		}
+	};
+
+	function btnCartChecked( $button ) {
+		$button.addClass( 'btn-cart--added' );
+
+		$button.find( '.btn-cart__icon' )
+			   .removeClass()
+			   .addClass('fa fa-check btn-cart__icon');
+
+		if ( $button.hasClass( 'btn-cart--trending' ) ) {
+			$button.find( '.btn-cart__text' )
+				   .html( 'Added!' );
+		}
+	};	
 
 
 	/*
